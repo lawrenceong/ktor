9665d662530d2f1d8b92bb51cd3f8fbe93a21b2b
Leonid Stashevsky <leonid.stashevsky@gmail.com>
2018-08-13 14:58:00 +0300

    Add serialization service loader

--- ktor-client/ktor-client-features/ktor-client-json/ktor-client-json-jvm/src/io/ktor/client/features/json/DefaultJvm.kt
+++ ktor-client/ktor-client-features/ktor-client-json/ktor-client-json-jvm/src/io/ktor/client/features/json/DefaultJvm.kt
@@ -1,3 +1,6 @@
 package io.ktor.client.features.json
 
-actual fun defaultSerializer(): JsonSerializer = GsonSerializer()
+import java.util.*
+
+actual fun defaultSerializer(): JsonSerializer =
+    ServiceLoader.load(JsonSerializer::class.java).toList().first()
